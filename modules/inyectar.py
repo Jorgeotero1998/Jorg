1d10167b14ac4bcc5c98e814ff944f97dadee614
Jorge Otero <jorgotero4@gmail.com>
2026-04-16 07:03:14 -0300

    Update inyectar.py

--- modules/inyectar.py
+++ modules/inyectar.py
@@ -12,11 +12,10 @@ for day in range(total_days + 1):
     current_date = start_date + timedelta(days=day)
     date_str = current_date.strftime('%Y-%m-%dT12:00:00')
     
-    # Configuramos la fecha para este bloque de commits
     os.environ['GIT_AUTHOR_DATE'] = date_str
     os.environ['GIT_COMMITTER_DATE'] = date_str
     
-    # Ejecutamos los 78 commits del dia en un solo proceso de shell para ir rapido
+   
     cmd = f'for /L %j in (1,1,78) do git commit -m "Data entry %j - {date_str}" --allow-empty --quiet'
     subprocess.run(cmd, shell=True, env=os.environ)
     
